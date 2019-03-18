a26f0995544af91d0cc64cc73b7fa59f992359d1
Chris <chrislishuanghong@gmail.com>
2019-03-18 15:09:02 +1100

    moved create_index

--- create_text_index.sh
+++
@@ -1,4 +0,0 @@
-\export PATH="/mnt/applications/anaconda3/bin:$PATH"
-sh /mnt/applications/anaconda3/bin/activate
-jupyter nbconvert --to script create_text_index.ipynb
-python3 create_text_index.py
