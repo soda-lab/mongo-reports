e5652c7086dffb121570529f7f0b6c733c1a81ed
Chris <chrislishuanghong@gmail.com>
2019-04-01 16:28:05 +1100

    upload merge_csv

---
+++ merge_csv.sh
@@ -0,0 +1,9 @@
+# folder_name = hashtag_city
+# ip = config['DEFAULT']['IP']
+# port = config['DEFAULT']['MongoDB-Port']
+
+\export PATH="/mnt/applications/anaconda3/bin:$PATH"
+sh /mnt/applications/anaconda3/bin/activate
+jupyter nbconvert --to script merge_csv.ipynb
+python3 merge_csv.py
+cp output/merged_csv/*.csv /mnt/jerkins/userContent
