c0312c12507a313ced94e993b5736e1556ca7113
Chris <chrislishuanghong@gmail.com>
2019-04-05 12:29:07 +1100

    update: change name

--- merge_hashtag_city_csv.sh
+++ merge_hashtag_city_csv.sh
@@ -4,6 +4,6 @@
 
 \export PATH="/mnt/applications/anaconda3/bin:$PATH"
 sh /mnt/applications/anaconda3/bin/activate
-jupyter nbconvert --to script merge_csv.ipynb
-python3 merge_csv.py
+jupyter nbconvert --to script merge_hashtag_city_csv.ipynb
+python3 merge_hashtag_city_csv.py
 cp output/merged_csv/*.csv /mnt/jerkins/userContent
